3fd5feaed918ba674f918bf830a0e78ad125260a
MiguelRamirezAgustin <miguel.r.agustin@gmail.com>
2018-10-13 17:49:55 -0500

    vistas

--- app/controllers/viewSelect.js
+++ app/controllers/viewSelect.js
@@ -1,5 +1,11 @@
 // Arguments passed into this controller can be accessed via the `$.args` object directly or:
 
+var label = Titanium.UI.createLabel({
+    text:"Seleccionado....",
+    width:"auto",
+    height:"auto"
+});
+
 
 var colorDialog = Titanium.UI.createOptionDialog({
     title:"Cual es tu color favorito?",
@@ -7,12 +13,6 @@ var colorDialog = Titanium.UI.createOptionDialog({
     cancel:2
 });
 
-var optionButton = Titanium.UI.createButton({
-    title:"Option Dialog!",
-    height:48,
-    width:120,
-    bottom:12
-});
 
 var view1= Ti.UI.createView({
 	height:"100%",
@@ -29,17 +29,14 @@ var view2= Ti.UI.createView({
 colorDialog.addEventListener("click",function(e){
     if(e.index < 2){
         if(e.index === 0){
-        	$.parent.add($.view1);
-            $.parent.remove($.view2); 
-         //$.btn_color.style.setBackgroundColor=("red");
+        	$.container.add($.view1);
         }else if(e.index === 1){
-          $.btn_color.style.setBorderColor=('red');
+        	$.parent.remove($.view2);
         }
     }else{
-        label.text = "Aww shucks...";
+        label.text = ".......";
     }
 });
-
 
 $.Dialogo.addEventListener("click",function(e){
     colorDialog.show();
