1f84e5706fa2dc52f0d204584445966f274ccbce
MiguelRamirezAgustin <miguel.r.agustin@gmail.com>
2018-10-13 21:40:29 -0500

    correcion de optionDialogo para cambiar color de view

--- app/controllers/viewSelect.js
+++ app/controllers/viewSelect.js
@@ -9,8 +9,7 @@ var label = Titanium.UI.createLabel({
 
 var colorDialog = Titanium.UI.createOptionDialog({
     title:"Cual es tu color favorito?",
-    options: ["Amarillo","Rosa","Ninguno"],
-    cancel:2
+    options: ["Amarillo","Rojo",'Azul','Verde','Naranja',"Ninguno"],
 });
 
 
@@ -19,24 +18,44 @@ var view1= Ti.UI.createView({
 	width:"100%",
 	backgroundColor:"red"
 });
+
 var view2= Ti.UI.createView({
 	height:"100%",
 	width:"100%",
 	backgroundColor:"blue"
 });
 
+//mandar a llamar otra vista dependiendo la opcion del optiondialogo
+// colorDialog.addEventListener("click",function(e){
+//     if(e.index == 0){
+//             $.parent.add(view1);
+//             $.parent.remove(view2);
+//         }else if(e.index == 1){
+//             $.parent.remove(view2);
+//             $.parent.add(view1);
+//         }else if(e.index == 2){
+//         	$.container.setBackgroundColor('pink');
+//         }
+// });
 
+
+//cambiar de color una vista
 colorDialog.addEventListener("click",function(e){
-    if(e.index < 2){
-        if(e.index === 0){
-        	$.container.add($.view1);
-        }else if(e.index === 1){
-        	$.parent.remove($.view2);
+    if(e.index == 0){
+            $.container.setBackgroundColor('yellow');
+        }else if(e.index == 1){
+            $.container.setBackgroundColor('red');
+        }else if(e.index == 2){
+        	$.container.setBackgroundColor('blue');
+        }else if(e.index==3){
+            $.container.setBackgroundColor('#00CC33');
+        }else if(e.index==4){
+            $.container.setBackgroundColor('Orange');
+        }else if(e.index==5){
+            alert("No se cambio el color")
         }
-    }else{
-        label.text = ".......";
-    }
 });
+
 
 $.Dialogo.addEventListener("click",function(e){
     colorDialog.show();
